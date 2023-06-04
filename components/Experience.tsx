5c197b605143f1e2d15ed89301ed896aa67eba5c
Harsh <harshjoshi2k@gmail.com>
2023-06-04 17:19:10 +0530

    first-commit

---
+++ components/Experience.tsx
@@ -0,0 +1,214 @@
+"use client";
+import React from "react";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import Typography from "@mui/material/Typography";
+import SlideUp from "./SlideUp";
+import { BsFillBuildingFill } from "react-icons/bs";
+import { BsFillBriefcaseFill } from "react-icons/bs";
+const Experience = () => {
+  return (
+    <section id="experience">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Experience
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+      <div className="hidden flex-col space-y-28 md:flex">
+        <SlideUp offset="-300px 0px -300px 0px">
+          <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+            <Timeline position="alternate">
+              <TimelineItem>
+                <TimelineOppositeContent
+                  sx={{ m: "auto 0", fontWeight: 700 }}
+                  align="right"
+                  variant="h5"
+                  color="white"
+                >
+                  2017-2019
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineConnector className="h-16" />
+                  <TimelineDot color="primary">
+                    <BsFillBuildingFill />
+                  </TimelineDot>
+                  <TimelineConnector className="h-16" />
+                </TimelineSeparator>
+                <TimelineContent sx={{ py: "12px", px: 2 }}>
+                  <Typography
+                    variant="h6"
+                    component="span"
+                    fontWeight={700}
+                    letterSpacing="1px"
+                  >
+                    Bal Bhavan Public School
+                  </Typography>
+                  <Typography letterSpacing="1px">
+                    Graduated my high school with{" "}
+                    <span className="font-extrabold">82%</span> and with many
+                    sports tournament certificates.
+                  </Typography>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent
+                  sx={{ m: "auto 0", fontWeight: 700 }}
+                  variant="h5"
+                  color="white"
+                >
+                  2019-2022
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineConnector className="h-16" />
+                  <TimelineDot color="primary">
+                    <BsFillBuildingFill />
+                  </TimelineDot>
+                  <TimelineConnector className="h-16" />
+                </TimelineSeparator>
+                <TimelineContent sx={{ py: "12px", px: 2 }}>
+                  <Typography variant="h6" component="span" fontWeight={700}>
+                    IIMT Affiliated to GGSIPU
+                  </Typography>
+                  <Typography>
+                    Graduated my college with{" "}
+                    <span className="font-extrabold">8.4 CGPA</span> with the
+                    knowledge of business and computer science
+                  </Typography>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent
+                  sx={{ m: "auto 0", fontWeight: 700 }}
+                  variant="h5"
+                  color="white"
+                >
+                  2020-2022
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineConnector className="h-16" />
+                  <TimelineDot color="primary">
+                    <BsFillBriefcaseFill />
+                  </TimelineDot>
+                  <TimelineConnector
+                    sx={{ bgcolor: "white" }}
+                    className="h-16"
+                  />
+                </TimelineSeparator>
+                <TimelineContent sx={{ py: "12px", px: 2 }}>
+                  <Typography variant="h6" component="span" fontWeight={700}>
+                    COSP Technologies Pvt. Ltd.
+                  </Typography>
+                  <Typography>
+                    Worked as full-time Data Manager and Fullstack Development
+                    Intern learned about
+                    <span className="font-bold"> Data Cleaning</span> using{" "}
+                    <span className="font-bold">Ms Excel</span> and creating
+                    API's using <span className="font-bold">Python</span> and
+                    frontend development with{" "}
+                    <span className="font-bold">React</span>
+                  </Typography>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent
+                  sx={{ m: "auto 0", fontWeight: 700 }}
+                  variant="h5"
+                  color="white"
+                >
+                  2022-Present
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineConnector
+                    sx={{ bgcolor: "white" }}
+                    className="h-16"
+                  />
+                  <TimelineDot color="primary">
+                    <BsFillBriefcaseFill />
+                  </TimelineDot>
+                  <TimelineConnector className="h-16" />
+                </TimelineSeparator>
+                <TimelineContent sx={{ py: "12px", px: 2 }}>
+                  <Typography variant="h6" component="span" fontWeight={700}>
+                    SNVA Ventures
+                  </Typography>
+                  <Typography>
+                    Wroking as a fulltime MERN Stack developer with the
+                    responsibilities of creating and integrating API's using{" "}
+                    <span className="font-bold">Node JS</span>,
+                    <span className="font-bold">Express JS</span>, develop
+                    frontend using <span className="font-bold">React JS</span>,
+                    <span className="font-bold">Next JS</span> and maintain
+                    database using<span className="font-bold"> Mongodb</span>
+                  </Typography>
+                </TimelineContent>
+              </TimelineItem>
+            </Timeline>
+          </div>
+        </SlideUp>
+      </div>
+      <div className="flex flex-col space-y-28 md:hidden">
+        <SlideUp offset="-300px 0px -300px 0px">
+          <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+            <Timeline>
+              <TimelineItem>
+                <TimelineSeparator>
+                  <TimelineDot />
+                  <TimelineConnector className="h-16" />
+                  <TimelineConnector className="h-16" />
+                  <TimelineConnector className="h-16" />
+                </TimelineSeparator>
+                <TimelineContent>
+                  <Typography variant="body1" fontWeight={600}>
+                    Bal Bhavan Public School (2017-2019)
+                  </Typography>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineSeparator>
+                  <TimelineDot />
+                  <TimelineConnector className="h-16" />
+                  <TimelineConnector className="h-16" />
+                  <TimelineConnector className="h-16" />
+                </TimelineSeparator>
+                <TimelineContent>
+                  <Typography variant="body1" fontWeight={600}>
+                    IIMT Affiliated to GGSIPU (2019-2022)
+                  </Typography>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineSeparator>
+                  <TimelineDot />
+                </TimelineSeparator>
+                <TimelineContent>
+                  <Typography variant="body1" fontWeight={600}>
+                    COSP Technologies Pvt. Ltd. (2020-2022)
+                  </Typography>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineSeparator>
+                  <TimelineDot />
+                  <TimelineConnector className="h-16" />
+                  <TimelineConnector className="h-16" />
+                  <TimelineConnector className="h-16" />
+                </TimelineSeparator>
+                <TimelineContent>
+                  <Typography variant="body1" fontWeight={600}>
+                    SNVA Ventures (2022-Present)
+                  </Typography>
+                </TimelineContent>
+              </TimelineItem>
+            </Timeline>
+          </div>
+        </SlideUp>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
---
+++ components/ProjectsSection.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import SlideUp from "./SlideUp";
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
+
+const projects = [
+  {
+    name: "Rocket AI",
+    description:
+      "Rocket AI is a web app that helps you find AI Tools and website according to your need, using MERN stack with latest version of Next 13, Tailwind and Tedux Toolkit.",
+    image: "/AI-project.png",
+    github: "https://github.com/hqasmei/thankful-thoughts",
+    link: "#",
+  },
+  {
+    name: "Blogger Hub",
+    description:
+      "Typical Blog Website where you can read write and upload blogs, using MERN Stack, MUI,React Router DOM and Redux Toolkit.",
+    image: "/blog-project.png",
+    github: "https://github.com/Sandorcleagne/blogfrontend",
+    link: "#",
+  },
+  {
+    name: "Acme Rocket 🚀",
+    description:
+      "Static Website with the latest version of Next 13 and tailwind css.",
+    image: "/tailwind-project.png",
+    github: "https://github.com/Sandorcleagne/Tailwind-Project",
+    link: "https://tailwind-project-rho.vercel.app/",
+  },
+];
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className=" md:w-1/2">
+                    <Link href={project.link}>
+                      <Image
+                        src={project.image}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                    </Link>
+                  </div>
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                      <Link href={project.link} target="_blank">
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsSection;
